454929215eb1b1649908717b236db01810877ec5
Dodti-pro <yoyoswag2024@gmail.com>
2025-11-11 22:43:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Listening to 𝗥𝗔𝗗𝗢 𝗚𝗔𝗠𝗜𝗡𝗚", "Playing 𝗪𝗵𝗶𝘁𝗲𝗹𝗶𝘀𝘁", "Watching 300+ 𝗠𝗲𝗺𝗯𝗲𝗿𝘀"];
-const statusTypes = [ 'dnd', 'idle', 'online'];
+const statusMessages = ["Watching MineLounge Members "];
+const statusTypes = ['online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
